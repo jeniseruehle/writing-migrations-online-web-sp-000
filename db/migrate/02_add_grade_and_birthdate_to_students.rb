04170ee5efe74042c1368b7ab4d496439f023469
Jenise Ruehle <jela07@gmail.com>
2020-08-12 18:59:16 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,7 +2,8 @@ class AddGradeAndBirthdateToStudents
   
   def change
     create_table :students do |t|
-      t.string :name 
+      t.integer :grade 
+      t.string :birthdate
     end 
   end 
 end 
